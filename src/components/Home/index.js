0e48cb12279052c6530f7ea25bcc99bccf703ec4
nizaralydrus <nizarmonzaa@gmail.com>
2022-10-21 20:37:29 -0500

    on process

---
+++ src/components/Home/index.js
@@ -0,0 +1,37 @@
+import LogoTitle from '../../assets/images/logo-s.png';
+import { Link } from 'react-router-dom';
+import './index.scss';
+
+
+
+
+
+
+const Home = () => {
+
+    return(
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br/> I'm
+                <img src={LogoTitle} alt="developer" />
+                monza
+                <br />
+                web developer
+                </h1>
+                <h2>
+                    Frontend Developer / JavaScript
+                </h2>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+            </div>
+        </div>
+
+    );
+}
+
+
+
+
+
+
+
+export default Home;
